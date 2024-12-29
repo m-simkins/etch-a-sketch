7c0a57e2ba12312e28935f6407b2728941b9881f
mike simkins <185857345+m-simkins@users.noreply.github.com>
2024-12-28 22:20:16 -0500

    add hover capability

--- script.js
+++ script.js
@@ -1,55 +1,36 @@
-setup();
+const createGridButton = document.createElement("button");
+createGridButton.id = "createGridButton";
+createGridButton.innerText = "create grid";
+document.body.appendChild(createGridButton);
 
-function setup() {
-    const inputContainer = document.createElement("div");
-    inputContainer.id = "inputContainer";
-    document.body.appendChild(inputContainer);
+const gridContainer = document.createElement("div");
+gridContainer.id = "gridContainer";
+document.body.appendChild(gridContainer);
 
-    const gridContainer = document.createElement("div");
-    gridContainer.id = "gridContainer";
-    document.body.appendChild(gridContainer);
-        
-    const gridSizeLabel = document.createElement("label");
-    gridSizeLabel.setAttribute("for", "gridSizeInput");
-    gridSizeLabel.innerText = "grid size:";
-    inputContainer.appendChild(gridSizeLabel);
-    
-    const gridSizeInput = document.createElement("input");
-    gridSizeInput.type = "number";
-    gridSizeInput.id = "gridSizeInput";
-    inputContainer.appendChild(gridSizeInput);
-    
-    const createGridButton = document.createElement("button");
-    createGridButton.id = "createGridButton";
-    createGridButton.innerText = "create grid";
-    inputContainer.appendChild(createGridButton);
-
-    createGridButton.addEventListener("click", () => {
-        if (gridSizeInput.value < 1) {
-            alert("enter a number greater than 0");
-        } else {
-            gridContainer.replaceChildren();
-            createGrid(gridSizeInput.value);
-        }
-    })
-}
+createGridButton.addEventListener("click", () => {
+    const gridSize = Number(prompt("set grid size"));
+    console.log(gridSize);
+    if (isNaN(gridSize) || gridSize < 1) {
+        alert("invalid")
+    } else {
+        gridContainer.replaceChildren();
+        createGrid(gridSize);
+    }
+})
 
 function createGrid(gridSize) {
     for (let i = 0; i < gridSize; i++) {
         const gridColumn = document.createElement("div");
         gridColumn.classList.add("grid-column");
-        document.getElementById("gridContainer").appendChild(gridColumn);
-    }
-    createCells(gridSize);
-}
+        gridContainer.appendChild(gridColumn);
 
-function createCells(gridSize) {
-    const gridColumns = document.getElementsByClassName("grid-column");
-    for (let column of gridColumns) {
-        for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
             const gridCell = document.createElement("div");
             gridCell.classList.add("grid-cell");
-            column.appendChild(gridCell);
+            gridColumn.appendChild(gridCell);
+            gridCell.addEventListener("mouseenter", () => {
+                gridCell.style.backgroundColor = "#999";
+            })
         }
     }
 }
