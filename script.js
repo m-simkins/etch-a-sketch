4508e980257f3aa60892a9bf4c254df88ae3034b
mike simkins <185857345+m-simkins@users.noreply.github.com>
2024-12-28 21:43:56 -0500

    create grid

--- script.js
+++ script.js
@@ -1,17 +1,55 @@
-const container = document.getElementById("container");
-const containerStyles = window.getComputedStyle(container);
-const containerWidth = containerStyles.getPropertyValue("width");
+setup();
 
-let gridSize = 16;
+function setup() {
+    const inputContainer = document.createElement("div");
+    inputContainer.id = "inputContainer";
+    document.body.appendChild(inputContainer);
 
-const gridSquare = document.querySelector(".grid-square");
-const gridSquareStyles = window.getComputedStyle(gridSquare);
-gridSquare.style.width = `calc(${containerWidth} / ${gridSize}})`
-const gridSquareWidth = gridSquareStyles.getPropertyValue("width");
+    const gridContainer = document.createElement("div");
+    gridContainer.id = "gridContainer";
+    document.body.appendChild(gridContainer);
+        
+    const gridSizeLabel = document.createElement("label");
+    gridSizeLabel.setAttribute("for", "gridSizeInput");
+    gridSizeLabel.innerText = "grid size:";
+    inputContainer.appendChild(gridSizeLabel);
+    
+    const gridSizeInput = document.createElement("input");
+    gridSizeInput.type = "number";
+    gridSizeInput.id = "gridSizeInput";
+    inputContainer.appendChild(gridSizeInput);
+    
+    const createGridButton = document.createElement("button");
+    createGridButton.id = "createGridButton";
+    createGridButton.innerText = "create grid";
+    inputContainer.appendChild(createGridButton);
 
-console.log(containerWidth);
-console.log(gridSquareWidth);
+    createGridButton.addEventListener("click", () => {
+        if (gridSizeInput.value < 1) {
+            alert("enter a number greater than 0");
+        } else {
+            gridContainer.replaceChildren();
+            createGrid(gridSizeInput.value);
+        }
+    })
+}
 
-const squareCount = gridSize * gridSize;
+function createGrid(gridSize) {
+    for (let i = 0; i < gridSize; i++) {
+        const gridColumn = document.createElement("div");
+        gridColumn.classList.add("grid-column");
+        document.getElementById("gridContainer").appendChild(gridColumn);
+    }
+    createCells(gridSize);
+}
 
-
+function createCells(gridSize) {
+    const gridColumns = document.getElementsByClassName("grid-column");
+    for (let column of gridColumns) {
+        for (let i = 0; i < gridSize; i++) {
+            const gridCell = document.createElement("div");
+            gridCell.classList.add("grid-cell");
+            column.appendChild(gridCell);
+        }
+    }
+}
